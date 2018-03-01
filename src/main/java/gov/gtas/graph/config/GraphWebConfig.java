d6e7f139bb146eaf2941fc075d0f238b731df7da
Alex Donley <donleyac@gmail.com>
2018-03-01 14:58:10 -0500

    Added Frontend to static folder

--- src/main/java/gov/gtas/graph/config/GraphWebConfig.java
+++ src/main/java/gov/gtas/graph/config/GraphWebConfig.java
@@ -3,6 +3,8 @@ package gov.gtas.graph.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -12,5 +14,14 @@ public class GraphWebConfig extends WebMvcConfigurerAdapter {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
            .allowCredentials(false).maxAge(3600);
+    }
+    @Override
+    public void addResourceHandlers (ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/static/");
+    }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/static/index.html");
     }
 }
